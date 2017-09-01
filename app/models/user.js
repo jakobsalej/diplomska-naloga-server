871e34368e4cb6fddb911d6ad4a59b41ba78c082
Jakob Salej <jakob.salej@gmail.com>
2017-09-01 02:12:22 +0200

    User entity added to DB

--- app/models/user.js
+++ app/models/user.js
@@ -20,4 +20,16 @@ var UserSchema = new Schema({
   	}
 });
 
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
 module.exports = mongoose.model('User', UserSchema);
